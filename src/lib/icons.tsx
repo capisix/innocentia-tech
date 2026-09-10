176b90ad6d9d222b905ec2c3c31bef9223f65ddf
Innocentia Tech <admin@innocentia.tech>
2026-09-10 02:09:35 -0600

    fix(icons): remover declaracion duplicada de TrendingDown que provocaba fallo de compilacion en Vercel

--- src/lib/icons.tsx
+++ src/lib/icons.tsx
@@ -598,10 +598,3 @@ export const Download = createIcon(
   </g>
 );
 
-export const TrendingDown = createIcon(
-  <g>
-    <polyline points="23 18 13.5 8.5 8.5 13.5 1 6" />
-    <polyline points="17 18 23 18 23 12" />
-  </g>
-);
-
